fc1bd89d98a71775ba0d18cb616aeb7bd7d4b8ac
Ahmed-Saad-J <ahmed.sa3dj@gmail.com>
2024-04-26 14:22:37 +0300

    Upload images

---
+++ src/app/_components/topNav.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { UploadButton } from "../utils/uploadthing";
+import { useRouter } from "next/navigation";
+
+//topNav
+export function TopNav(){
+  const router = useRouter();
+    return(
+    <nav className="flex w-full items-center justify-between p-4 text-xl font-semibold border-b">
+      <div>Gellary</div>
+      <div className="flex flex-row">
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UploadButton  endpoint="imageUploader" onClientUploadComplete={()=>{router.refresh()}}/>
+        <UserButton />
+      </SignedIn>
+      </div>
+      
+    </nav>
+    )
+  }
